b76a858f1a3f1dc1da2ea2802e36bdcd4d6e0441
acastemoreno <acastemoreno@gmail.com>
2018-09-18 12:54:42 -0500

    end value

--- lib/prueba_web/controllers/pi_controller.ex
+++ lib/prueba_web/controllers/pi_controller.ex
@@ -5,9 +5,10 @@ defmodule PruebaWeb.PiController do
     render(conn, "attribute_form.html")
   end
 
-  def attribute_form_post(conn, %{"form" => %{"tag" => tag}}) do
-    case Prueba.Pi.Attributes.webid(%{path: tag}) do
-      {:ok, webid} -> text conn, webid
+  def attribute_form_post(conn, %{"form" => %{"path" => path, "operation" => operation}}) do
+    atom = String.to_atom(operation)
+    case apply(Prueba.Pi.Attributes, atom, [%{path: path}]) do
+      {:ok, msg} -> text conn, msg
       {:error, msg} -> text conn, msg
     end
   end
