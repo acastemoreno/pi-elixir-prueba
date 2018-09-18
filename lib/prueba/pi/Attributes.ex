54d523628244cfb821f99a3fb88498eb3bc04c35
acastemoreno <acastemoreno@gmail.com>
2018-09-18 12:41:28 -0500

    end value

--- lib/prueba/pi/Attributes.ex
+++ lib/prueba/pi/Attributes.ex
@@ -4,21 +4,17 @@ defmodule Prueba.Pi.Attributes do
   import Prueba.Pi.HttpClient, only: [headers: 0, options: 0]
 
   # Client
-
-  def webid(%{path: path}) when path |> is_bitstring() do
-    GenServer.call(__MODULE__, {:webid, %{path: path}})
-  end
-
-  def webid(_path) do
-    {:error, "Argumento no valido"}
-  end
-
   def start_link() do
     GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
 
+  def webid(%{path: path}) when path |> is_bitstring() do
+    GenServer.call(__MODULE__, {:webid, %{path: path}})
+  end
+  def webid(_path), do: {:error, "Argumento no valido"}
+
   def value(%{path: path}) when path |> is_bitstring() do
-    GenServer.call(__MODULE__, {:value_by_path, path})
+    GenServer.call(__MODULE__, {:value, %{path: path}})
   end
 
   def value(_path) do
@@ -39,17 +35,34 @@ defmodule Prueba.Pi.Attributes do
     {:reply, response, state}
   end
 
+  def handle_call({:value, %{path: path}}, _from, state) do
+    {response, state} = Map.get(state, path)
+      |> request_webid_and_update_state_if_necesary(path, state)
+      |> request_value()
+    {:reply, response, state}
+  end
+
   # Helper Functions
+
+  defp request_value({{:error, _msg}, _state} = response_state), do: response_state
+  defp request_value({{:ok, webid}, state}) do
+    {{:ok, get_current_value(%{webid: webid})}, state}
+  end
 
   defp request_webid_and_update_state_if_necesary(nil, path, state) do
     case get_webid_and_status(%{path: path}) do
       %{webid: webid, status_code: 200} ->
+        IO.puts("WebID se actualiza")
         {{:ok, webid}, state |> Map.put(path, webid)}
       _ ->
+        IO.puts("bad_request")
         {{:error, "bad_request"}, state}
     end
   end
-  defp request_webid_and_update_state_if_necesary(webid, _path, state), do: {{:ok, webid}, state}
+  defp request_webid_and_update_state_if_necesary(webid, _path, state) do
+      IO.puts("WebID no se actualiza")
+     {{:ok, webid}, state}
+  end
 
   defp get_webid_and_status(%{path: path}) do
     %{body: body, status_code: status_code} =
@@ -58,9 +71,11 @@ defmodule Prueba.Pi.Attributes do
     %{webid: body |> Keyword.get(:"WebId"), status_code: status_code}
   end
 
-  defp current_value(webid) do
-    ("streams/" <> webid <>"/end")
-    |> HttpClient.get!(headers(), options())
+  defp get_current_value(%{webid: webid}) do
+    %{body: body, status_code: 200} =
+      ("streams/" <> webid <>"/end")
+      |> HttpClient.get!(headers(), options())
+    body |> Keyword.get(:"Value")
   end
 
 end
