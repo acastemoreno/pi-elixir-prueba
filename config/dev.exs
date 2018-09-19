5afd75cb30f8251e5aea52b27483702dc917cb15
acastemoreno <acastemoreno@gmail.com>
2018-09-19 15:36:54 -0500

    return no brunch

--- config/dev.exs
+++ config/dev.exs
@@ -11,13 +11,8 @@ config :reporte_pi, ReportePiWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--watch-stdin --progress --color",
-      cd: Path.expand("../assets", __DIR__)
-    ]
-  ]
+  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
+                    cd: Path.expand("../assets", __DIR__)]]
 
 # ## SSL Support
 #
